fffe36d930a0478c4db73181b99cc9288e403380
N Christopher <nwillc@gmail.com>
2015-06-08 13:38:33 -0400

    Starting Heroku setup

--- run.sh
+++ run.sh
@@ -6,9 +6,4 @@ cd ${SCRIPT_DIR}
 
 mvn install -DskipTests
 
-LIB_DIR=./target/dependency
-
-export CLASSPATH=$(JARS=(${LIB_DIR}/*.jar); IFS=: ; echo "${JAR_FILE}:${JARS[*]}")
-CLASSPATH=$(ls ./target/shields*.jar)${CLASSPATH}
-
-java com.github.nwillc.shields.Shields
+java -cp target/classes:target/dependency/* com.github.nwillc.shields.Shields
